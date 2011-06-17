d2a9105ce2b7ad6c84f2aa67ece273a1feeab90b
nethsix <nethsix@gmail.com>
2011-06-17 21:23:58 +0900

    Heroku crashing because of unwanted but undeleted models?
    We delete old models from git.

--- app/models/entity_201106012.rb
+++
@@ -1,8 +0,0 @@
-require 'validators'
-
-class Entity < ActiveRecord::Base
-
-  validates :doc_id, :presence => true, :uniqueness => true, :doc_id_foreign_key => true
-  validates :status, :presence => true, :status_type => true
-
-end
